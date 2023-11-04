904a36de7ffc3788a7c4a42bea44900351e724db
unknown <hzfjcu123@163.com>
2023-11-04 17:00:53 +0800

    弹幕

--- js/包装类/5.js
+++ js/包装类/5.js
@@ -1,44 +1,44 @@
-var obj = {}
-console.log(obj.a)
+// var obj = {}
+// console.log(obj.a)
 
 
-var num = 123
-num.abc = 'hello'
-console.log(num.abc);
+// var num = 123
+// num.abc = 'hello'
+// console.log(num.abc);
 
-var num = new Number(123)
-num.abc = 'hello'
-console.log(num.abc);
-console.log(num * 2);//参与运算时会变数字
+// var num = new Number(123)
+// num.abc = 'hello'
+// console.log(num.abc);
+// console.log(num * 2);//参与运算时会变数字
 
-var str = 'abcd'
-
-
-var num = 4
-num.len = 3
+// var str = 'abcd'
 
 
-var num = new Number(4)
-num.len = 3
-delete num.len
+// var num = 4
+// num.len = 3
 
-new Number(4).len//隐式包装类
-console.log(num.len)
+
+// var num = new Number(4)
+// num.len = 3
+// delete num.len
+
+// new Number(4).len//隐式包装类
+// console.log(num.len)
 
 
 
 var arr = [1,2,3,4,5]
 arr.length = 2
-console.log(arr);
+console.log(arr.length);
 
-var str = 'abcd'
-//其实console.log -- 这个new String('abcd').length
+// var str = 'abcd'
+// //其实console.log -- 这个new String('abcd').length
 
-str.length = 2  
-console.log(str.length)
+// str.length = 2  
+// console.log(str.length)
 
 
-//面试题
+面试题
 var str = 'abc'
 str += 1
 var test = typeof(str)//'string'
