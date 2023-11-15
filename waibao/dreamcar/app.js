528e1f2406db3e3dec0f29095e18b4b5fec9c7fe
unknown <hzfjcu123@163.com>
2023-11-15 21:44:48 +0800

    小程序

--- waibao/dreamcar/app.js
+++ waibao/dreamcar/app.js
@@ -1,19 +1,22 @@
-// app.js
-App({
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+// 应用
+// page()页面
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+App({
+  globalData: {
+   
+  },
+  onLaunch() {
+    //在应用启动时，发送接口请求
+    // console.log('应用启动了')
+    wx.request({
+      url: 'https://resources.ninghao.net/wxapp-case/db.json',
+      success: (response) => {
+        console.log(response)
+        Object.assign(this.globalData,
+          response.data)
+          // console.log(this,'-----');
       }
     })
-  },
-  globalData: {
-    userInfo: null
   }
+
 })
---
+++ waibao/dreamcar/readme.md
@@ -0,0 +1,32 @@
+- app和page的概念
+  app 全局，和pages共享
+    app.js共享 globalData
+    app.wxss样式
+    app.json 应用的配置
+    globalData 如何来到page中
+    const app = getApp()
+    应用对象
+    data 初始化占位符
+    {
+      data:{
+        user{}
+      }
+    }
+   - 生命周期 
+   数据来自于后端接口
+   onLoad wxml + wxss 显示了
+   这是请求数据的最佳时机
+  - this.setData({
+    user:真正的值
+  }) 
+  - 页面会热更新 重新绘制{{}}这部分
+  -循环输出的概念
+   -模板在{{直接输出}}外，数组列表输出的常态
+   - block 只承载指令 wx:for
+     不会出现在文档流中
+     在循环的每一项item都是默认:item.image
+     -css架构思路
+     -app.wxss全局样式
+      -公用的样式
+      -模块化
+     - page.wxss 页面 
